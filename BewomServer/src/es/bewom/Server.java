9153aeb0f935c6a7be36833ccc3c37e7bd42e1cd
pagoru <pagoru@gmail.com>
2015-09-17 20:56:08 +0200

    linux

--- BewomServer/src/es/bewom/Server.java
+++ BewomServer/src/es/bewom/Server.java
@@ -14,7 +14,8 @@ public class Server {
 		lastStart = new Date().getTime()/1000;
 		try {
 			// String[] run = {"xterm", "-e", "java", "-jar", "server.jar", "nogui"}; 
-			String[] run = {"cmd", "/c", "start", "cmd.exe"}; 
+//			String[] run = {"cmd", "/c", "start", "cmd.exe"}; 
+			String[] run = {"xterm", "-e", "java", "-jar", "server.jar", "nogui"}; 
 			start = Runtime.getRuntime().exec(run);
 		} catch (IOException e) {
 			e.printStackTrace();
